8691613f9e2c76e2b5a786c099a656dbe617a1c0
Maksim Stankevic <maksim.stankevic1@gmail.com>
2025-05-02 14:26:09 +0300

    complete demo functionality and all native logging

--- internal/controller/ekswatch_controller.go
+++ internal/controller/ekswatch_controller.go
@@ -32,6 +32,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/secretsmanager"
 	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/plumbing/protocol/packp/capability"
 	"github.com/go-git/go-git/v5/plumbing/transport"
 	"github.com/go-git/go-git/v5/plumbing/transport/http"
@@ -103,34 +104,40 @@ func (r *EkswatchReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 	if len(ekswatch.Spec.ClustersToSyncRegexList) > 0 {
 		logging.Info("Clusters to sync regex list", "regexes", ekswatch.Spec.ClustersToSyncRegexList)
 
-		// Opens an already existing repository.
-		r, err := git.PlainOpen(repoDir)
-		if err != nil && err.Error() == "repository does not exist" {
-			logging.Info("No repo in place, cloning repository")
-
-			transport.UnsupportedCapabilities = []capability.Capability{
-				capability.ThinPack,
-			}
-
-			var err error
-
-			r, err = git.PlainClone(repoDir, false, &git.CloneOptions{
-				// The intended use of a GitHub personal access token is in replace of your password
-				// because access tokens can easily be revoked.
-				// https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
-				Auth: &http.BasicAuth{
-					Username: "dummy", // yes, this can be anything except an empty string
-					Password: os.Getenv("PAT"),
-				},
-				URL:   ekswatch.Spec.GitRepository,
-				Depth: 1,
-			})
+		// Check if the repo directory exists
+		if _, err := os.Stat(repoDir); err == nil {
+			logging.Info("Repo directory exists, deleting it before new clone", "repoDir", repoDir)
+			// If the directory exists, delete it
+			err := DeleteFolder(repoDir, ctx)
 			if err != nil {
-				logging.Error(err, "Error cloning git repository")
+				logging.Error(err, "Error deleting repo directory")
 				return ctrl.Result{}, err
 			}
-			logging.Info("Cloned git repository", "repo", ekswatch.Spec.GitRepository)
+			logging.Info("Deleted repo directory", "repoDir", repoDir)
 		}
+
+		logging.Info("Cloning repository")
+
+		transport.UnsupportedCapabilities = []capability.Capability{
+			capability.ThinPack,
+		}
+
+		r, err := git.PlainClone(repoDir, false, &git.CloneOptions{
+			// The intended use of a GitHub personal access token is in replace of your password
+			// because access tokens can easily be revoked.
+			// https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
+			Auth: &http.BasicAuth{
+				Username: "dummy", // yes, this can be anything except an empty string
+				Password: os.Getenv("PAT"),
+			},
+			URL:   ekswatch.Spec.GitRepository,
+			Depth: 1,
+		})
+		if err != nil {
+			logging.Error(err, "Error cloning git repository")
+			return ctrl.Result{}, err
+		}
+		logging.Info("Cloned git repository", "repo", ekswatch.Spec.GitRepository)
 
 		ref, err := r.Head()
 		if err != nil {
@@ -157,17 +164,17 @@ func (r *EkswatchReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 		go func(i int, account ekstoolsv1alpha1.Account) {
 			defer wg.Done()
 			var sess *session.Session
-			sess, allAuthErrors[i] = getCredsViaSts(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), account.AccountID, account.RoleName)
+			sess, allAuthErrors[i] = getCredsViaSts(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), account.AccountID, account.RoleName, ctx)
 			if sess == nil {
 				return
 			}
-			allListingErrors[i] = listEKSClusters(sess, &allClusters[i])
+			allListingErrors[i] = listEKSClusters(sess, &allClusters[i], ctx)
 		}(i, account)
 	}
 
 	// Get creds for listing k8s secrets
 
-	sess, err := getCredsViaSts(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), ekswatch.Spec.K8sSecretsLocation.AccountId, ekswatch.Spec.K8sSecretsLocation.RoleName)
+	sess, err := getCredsViaSts(os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"), ekswatch.Spec.K8sSecretsLocation.AccountId, ekswatch.Spec.K8sSecretsLocation.RoleName, ctx)
 	if err != nil {
 		logging.Error(err, "Error getting STS creds for listing k8s secrets")
 		return ctrl.Result{}, err
@@ -180,7 +187,7 @@ func (r *EkswatchReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 		defer wg.Done()
 		// Get the list of k8s secrets
 		var err error
-		k8sSecrets, err = listSecrets(sess, ekswatch.Spec.K8sSecretsLocation.Region)
+		k8sSecrets, err = listSecrets(sess, ekswatch.Spec.K8sSecretsLocation.Region, ctx)
 		if err != nil {
 			logging.Error(err, "Error listing k8s secrets")
 			return err
@@ -232,7 +239,7 @@ func (r *EkswatchReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 			var lastAutoSyncSucceeded string = "n/a"
 
 			// Check if the cluster matches any of the regexes
-			matches, err := MatchesAnyRegex(ekswatch.Spec.ClustersToSyncRegexList, cluster)
+			matches, err := MatchesAnyRegex(ekswatch.Spec.ClustersToSyncRegexList, cluster, ctx)
 			if err != nil {
 				logging.Error(err, "Error matching regex for cluster: "+cluster)
 				return ctrl.Result{}, err
@@ -243,7 +250,7 @@ func (r *EkswatchReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 				logging.Info("Cluster matches regex, autosyncing", "cluster", cluster)
 				autoSynced = true
 				// Add the cluster to the YAML file
-				err := AddClusterIfNotExists(repoDir+"/values/clusters.yaml", cluster)
+				err := AddClusterIfNotExists(repoDir+"/values/clusters.yaml", cluster, ctx)
 				if err != nil {
 					logging.Error(err, "Error adding cluster to YAML file")
 					lastAutoSyncSucceeded = "no"
@@ -266,12 +273,24 @@ func (r *EkswatchReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 			})
 		}
 	}
-	// Set the status of the Ekswatch instance
+	// Set the status of Ekswatch instance
 	if err := r.Status().Update(ctx, &ekswatch); err != nil {
 		logging.Error(err, "unable to update Ekswatch status")
 		return ctrl.Result{}, err
 	}
 	logging.Info("Updated Ekswatch status", "status", ekswatch.Status)
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		logging.Error(err, "Error getting hostname, going with default")
+		hostname = "ekswatch-operator"
+	}
+
+	// Commit and push changes to the git repository
+	if err := CommitAndPushChanges(repoDir, "AutoSync by Ekswatch: "+time.Now().Format("2006-01-02 15:04:05 UTC"), hostname, os.Getenv("PAT"), ctx); err != nil {
+		logging.Error(err, "Error committing and pushing changes to git repository")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{RequeueAfter: 5 * time.Minute}, nil
 }
@@ -283,13 +302,16 @@ func (r *EkswatchReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Complete(r)
 }
 
-func listEKSClusters(sess *session.Session, clusters *[]string) error {
+func listEKSClusters(sess *session.Session, clusters *[]string, ctx context.Context) error {
+
+	logging := log.FromContext(ctx)
 
 	// Get all available regions
 	ec2Svc := ec2.New(sess)
 	regionsOutput, err := ec2Svc.DescribeRegions(&ec2.DescribeRegionsInput{})
 	if err != nil {
-		return fmt.Errorf("failed to describe regions: %w", err)
+		logging.Error(err, "failed to describe regions")
+		return err
 	}
 
 	// Iterate over all regions and list EKS clusters
@@ -301,7 +323,8 @@ func listEKSClusters(sess *session.Session, clusters *[]string) error {
 		for {
 			listClustersOutput, err := eksSvc.ListClusters(listClustersInput)
 			if err != nil {
-				return fmt.Errorf("failed to list clusters in region %s: %w", regionName, err)
+				logging.Error(err, "failed to list clusters in region %s", regionName)
+				return err
 			}
 
 			*clusters = append(*clusters, aws.StringValueSlice(listClustersOutput.Clusters)...)
@@ -316,7 +339,9 @@ func listEKSClusters(sess *session.Session, clusters *[]string) error {
 	return nil
 }
 
-func getCredsViaSts(accessKeyID string, secretAccessKey string, accountId string, roleToAssume string) (*session.Session, error) {
+func getCredsViaSts(accessKeyID string, secretAccessKey string, accountId string, roleToAssume string, ctx context.Context) (*session.Session, error) {
+
+	logging := log.FromContext(ctx)
 
 	// build complete role ARN
 	roleArn := fmt.Sprintf("arn:aws:iam::%s:role/%s", accountId, roleToAssume)
@@ -327,7 +352,8 @@ func getCredsViaSts(accessKeyID string, secretAccessKey string, accountId string
 		Region:      aws.String("eu-west-1"),
 	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to create AWS session: %w", err)
+		logging.Error(err, "failed to create AWS session")
+		return nil, err
 	}
 
 	// Assume the specified role
@@ -337,7 +363,8 @@ func getCredsViaSts(accessKeyID string, secretAccessKey string, accountId string
 		RoleSessionName: aws.String("ekswatch-session"),
 	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to assume role: %w", err)
+		logging.Error(err, "failed to assume role")
+		return nil, err
 	}
 
 	// Create a new session with the assumed role credentials
@@ -350,12 +377,15 @@ func getCredsViaSts(accessKeyID string, secretAccessKey string, accountId string
 		Region: aws.String("eu-west-1"),
 	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to create session with assumed role: %w", err)
+		logging.Error(err, "failed to create session with assumed role")
+		return nil, err
 	}
 	return sess, nil
 }
 
-func listSecrets(sess *session.Session, region string) ([]string, error) {
+func listSecrets(sess *session.Session, region string, ctx context.Context) ([]string, error) {
+
+	logging := log.FromContext(ctx)
 
 	// Create a new Secrets Manager client
 	svc := secretsmanager.New(sess, &aws.Config{Region: aws.String(region)})
@@ -370,7 +400,8 @@ func listSecrets(sess *session.Session, region string) ([]string, error) {
 		return !lastPage
 	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to list secrets: %w", err)
+		logging.Error(err, "failed to list secrets")
+		return nil, err
 	}
 
 	return secrets, nil
@@ -387,24 +418,29 @@ func appendSecret(secret string, secrets []string) []string {
 }
 
 // AddClusterIfNotExists checks if a cluster exists in the YAML file and adds it if not.
-func AddClusterIfNotExists(filePath string, clusterName string) error {
+func AddClusterIfNotExists(filePath string, clusterName string, ctx context.Context) error {
+
+	logging := log.FromContext(ctx)
+
 	// Open the YAML file
 	file, err := os.ReadFile(filePath)
 	if err != nil {
-		return fmt.Errorf("failed to read file: %w", err)
+		logging.Error(err, "failed to read file")
+		return err
 	}
 
 	// Parse the YAML content
 	var clustersFile ClustersFile
 	err = yaml.Unmarshal(file, &clustersFile)
 	if err != nil {
-		return fmt.Errorf("failed to parse YAML: %w", err)
+		logging.Error(err, "failed to parse YAML")
+		return err
 	}
 
 	// Check if the cluster already exists
 	for _, cluster := range clustersFile.Clusters {
 		if cluster.Name == clusterName {
-			fmt.Println("Cluster already exists in the YAML file.")
+			logging.Info("Cluster already exists in the YAML file.")
 			return nil
 		}
 	}
@@ -413,7 +449,7 @@ func AddClusterIfNotExists(filePath string, clusterName string) error {
 	newCluster := Cluster{
 		Name: clusterName,
 		Labels: map[string]string{
-			"env": "dev", // Example label, modify as needed
+			"env": "FromController", // Example label, modify as needed
 		},
 	}
 	clustersFile.Clusters = append(clustersFile.Clusters, newCluster)
@@ -421,26 +457,32 @@ func AddClusterIfNotExists(filePath string, clusterName string) error {
 	// Marshal the updated content back to YAML
 	updatedYAML, err := yaml.Marshal(&clustersFile)
 	if err != nil {
-		return fmt.Errorf("failed to marshal updated YAML: %w", err)
+		logging.Error(err, "failed to marshal updated YAML")
+		return err
 	}
 
 	// Write the updated YAML back to the file
 	err = os.WriteFile(filePath, updatedYAML, 0644)
 	if err != nil {
-		return fmt.Errorf("failed to write updated YAML to file: %w", err)
+		logging.Error(err, "failed to write updated YAML to file")
+		return err
 	}
 
-	fmt.Println("Cluster added successfully.")
+	logging.Info("Cluster added successfully.")
 	return nil
 }
 
 // MatchesAnyRegex checks if the clusterName matches any of the provided regex patterns.
-func MatchesAnyRegex(regexList []string, clusterName string) (bool, error) {
+func MatchesAnyRegex(regexList []string, clusterName string, ctx context.Context) (bool, error) {
+
+	logging := log.FromContext(ctx)
+
 	for _, pattern := range regexList {
 		// Compile the regex pattern
 		re, err := regexp.Compile(pattern)
 		if err != nil {
-			return false, fmt.Errorf("invalid regex pattern: %s, error: %w", pattern, err)
+			logging.Error(err, "invalid regex pattern: %s", pattern)
+			return false, err
 		}
 
 		// Check if the clusterName matches the regex
@@ -449,4 +491,87 @@ func MatchesAnyRegex(regexList []string, clusterName string) (bool, error) {
 		}
 	}
 	return false, nil
+}
+
+// CommitAndPushChanges stages, commits, and pushes changes in a Git repository.
+func CommitAndPushChanges(repoPath, commitMessage, username, token string, ctx context.Context) error {
+
+	logging := log.FromContext(ctx)
+
+	// Open the existing repository
+	repo, err := git.PlainOpen(repoPath)
+	if err != nil {
+		logging.Error(err, "failed to open git repository")
+		return err
+	}
+
+	// Get the working tree
+	worktree, err := repo.Worktree()
+	if err != nil {
+		logging.Error(err, "failed to get worktree")
+		return err
+	}
+
+	// Check for changes in the working tree
+	status, err := worktree.Status()
+	if err != nil {
+		logging.Error(err, "failed to get worktree status")
+		return err
+	}
+
+	if status.IsClean() {
+		logging.Info("No changes to commit.")
+		return nil
+	}
+
+	// Stage all changes
+	err = worktree.AddWithOptions(&git.AddOptions{All: true})
+	if err != nil {
+		logging.Error(err, "failed to stage changes")
+		return err
+	}
+
+	// Commit the changes
+	_, err = worktree.Commit(commitMessage, &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  username,
+			Email: fmt.Sprintf("%s@ekswatch.devops", username), // Replace with a valid email if needed
+			When:  time.Now(),
+		},
+	})
+	if err != nil {
+		logging.Error(err, "failed to commit changes")
+		return err
+	}
+
+	logging.Info("Changes committed successfully.")
+
+	// Push the changes to the origin
+	err = repo.Push(&git.PushOptions{
+		Auth: &http.BasicAuth{
+			Username: username, // This can be anything except an empty string
+			Password: token,    // Personal Access Token (PAT)
+		},
+	})
+	if err != nil {
+		logging.Error(err, "failed to push changes")
+		return err
+	}
+
+	logging.Info("Changes pushed to origin successfully.")
+	return nil
+}
+
+// DeleteFolder deletes the specified folder and all its contents.
+func DeleteFolder(folderPath string, ctx context.Context) error {
+
+	logging := log.FromContext(ctx)
+
+	err := os.RemoveAll(folderPath)
+	if err != nil {
+		logging.Error(err, "failed to delete folder")
+		return err
+	}
+	logging.Info("Folder %s deleted successfully.\n", folderPath)
+	return nil
 }
